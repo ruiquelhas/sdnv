590ba330c924ad74c47e2c1dd89de47fb4cb4791
Rui Quelhas <rfpquelhas@gmail.com>
2013-06-14 16:53:18 +0100

    Encoding is working in standalone mode but tests are failing

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var encodeBuffer = function (buffer) {
-  return new Buffer([0x95, 0x3C]);
+  var lastOctet = buffer[buffer.length - 1];
+  var negativeMSBOctet = lastOctet - 128;
+
+  buffer[buffer.length - 1] = negativeMSBOctet;
+  var bitToShift = lastOctet < 128 ? 0 : 1;
+
+  for (var i = 0, len = buffer.length - 1; i < len; i++) {
+    var currentOctet = buffer[i];
+    var positiveMSBOctect = (currentOctet * 2) + 128 + bitToShift;
+    bitToShift = buffer[i] < 128 ? 0 : 1;
+    buffer[i] = positiveMSBOctect;
+  }
+
+  // return new Buffer([0x95, 0x3C]);
+  return buffer;
 };
 
 var SDNV = function (buffer) { 
--- test/all.js
+++ test/all.js
@@ -14,18 +14,20 @@ test('make sure a valid buffer results in a valid SNDV', function (t) {
 
 test('make sure the SDNV is valid when no buffer is passed', function (t) {
   var emptyBufferSDNV = new SDNV(), expectedBuffer = new Buffer(1);
-  t.ok(emptyBufferSDNV instanceof SDNV, 'empty buffer should result in a default SDNV');
-  t.type(emptyBufferSDNV.buffer, "Buffer", 'default SDNV should contain buffer data');
+  t.ok(emptyBufferSDNV instanceof SDNV, 'empty buffer should result in a \
+    default SDNV');
+  t.type(emptyBufferSDNV.buffer, "Buffer", 'default SDNV should contain \
+    buffer data');
   t.end();
 });
 
 test('make sure the encoding works', function (t) {
   var encoded = SDNV.encode(buff), expected = new Buffer([0x95, 0x3C]);
   // test the sdnv contents
-  t.equal(sdnv.buffer.toString(), expected.toString(), 
+  t.equal(sdnv.buffer.toString('hex'), expected.toString('hex'), 
     'the SDNV buffer should match the expected one');
   // test the utility method
-  t.equal(encoded.toString(), expected.toString(), 
+  t.equal(encoded.toString('hex'), expected.toString('hex'), 
     'the encoded buffer by the utility method should match the expected one');
   t.end();
 });
