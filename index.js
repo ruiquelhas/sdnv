f5232b7b990b5f3c0999d7decffef0cfe3d3bce9
Rui Quelhas <rfpquelhas@gmail.com>
2013-06-14 11:05:29 +0100

    Add encoding utility method stub and appropriate tests

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-module.exports = function (buffer) { 
+var encodeBuffer = function (buffer) {
+  return new Buffer([0x95, 0x3C]);
+};
+
+var SDNV = function (buffer) { 
   if (buffer !== null) {
-    this.buffer = buffer;
+    this.buffer = encodeBuffer(buffer);
   } else {
     this.buffer = new Buffer(1);
   }
 };
+
+SDNV.encode = function (buffer) {
+  return encodeBuffer(buffer);
+};
+
+module.exports = SDNV;
--- test/all.js
+++ test/all.js
@@ -1,17 +1,23 @@
 var test = require('tap').test;
 var SDNV = require('../index');
 
-var buff = new Buffer(4);
+var buff = new Buffer([0x0A, 0xBC]);
 var sdnv = new SDNV(buff);
 
 test('make sure the SDNV is valid', function (t) {
   t.ok(sdnv instanceof SDNV, 'should be able to create an SDNV instance');
   t.ok(sdnv.buffer instanceof Buffer, 'should contain buffer data');
-  t.equal(sdnv.buffer, buff, 'should contain the buffer given as paramater');
   t.end();
 });
 
 test('make sure the encoding works', function (t) {
+  var encoded = SDNV.encode(buff), expected = new Buffer([0x95, 0x3C]);
+  // test the sdnv contents
+  t.equal(sdnv.buffer.toString(), expected.toString(), 'the sdnv buffer \
+    should match the expected one');
+  // test the utility method
+  t.equal(encoded.toString(), expected.toString(), 'the encoded buffer by \
+    the utility method should match the expected one');
   t.end();
 });
 
