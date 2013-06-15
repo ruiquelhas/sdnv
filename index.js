13e1230610c5ec3a2577e685c0e4b78ea18fe681
Rui Quelhas <rfpquelhas@gmail.com>
2013-06-15 16:24:49 +0100

    Add support for 8-bit best case scenario encoding

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
+var OCTET_DESCRIPTION_SIZE = 128;
+
+var isSingleOctect = function (buffer) { 
+  return buffer.length < 2; 
+};
+
+var hasMSBEqualToZero = function(buffer) { 
+  return buffer[0] < OCTET_DESCRIPTION_SIZE; 
+};
+
 var encodeBuffer = function (buffer) {
-  // var bufferCopy = new Buffer(buffer.length);
-  // buffer.copy(bufferCopy);
+  var bufferCopy = new Buffer(buffer.length);
+  buffer.copy(bufferCopy);
+
+  // best case scenario (single octet with MSB = 0)
+  if (isSingleOctect(bufferCopy) && hasMSBEqualToZero(bufferCopy)) {
+    return bufferCopy;
+  }
+
+  // worst case scenario (single octet with MSB = 1)
+  if (isSingleOctect(bufferCopy) && !hasMSBEqualToZero(bufferCopy)) {
+    return new Buffer([0x7F]);
+  }
+
+  return new Buffer([0x7F]);
 
   // var lastOctet = bufferCopy[bufferCopy.length - 1];
   // var negativeMSBOctet = lastOctet - 128;
-
   // bufferCopy[bufferCopy.length - 1] = negativeMSBOctet;
   // var bitToShift = lastOctet < 128 ? 0 : 1;
-
   // for (var i = 0, len = bufferCopy.length - 1; i < len; i++) {
   //   var currentOctet = bufferCopy[i];
   //   var positiveMSBOctect = (currentOctet * 2) + 128 + bitToShift;
   //   bitToShift = bufferCopy[i] < 128 ? 0 : 1;
   //   bufferCopy[i] = positiveMSBOctect;
   // }
-  
-  // return bufferCopy;
-  return new Buffer([0x7F]);
 };
 
 var SDNV = function (buffer) { 
